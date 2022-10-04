bca2e4816e4db8c9eadb9a16fc2f3867076ab3e8
osasinfo <osasinfo160@gmail.com>
2022-10-04 15:45:35 +0000

    fixBug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if args == 0:
         print("0 arguments.")
     elif args == 1:
-            print("1 argument:")
-        else:
-            print("{} arguments:".format(args))
-        for i in range(args):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args))
+    for i in range(args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     temp = dir(hidden_4)
     for temp in temp:
-            if temp[:2] != "__":
-                print(temp)
+        if temp[:2] != "__":
+            print(temp)
